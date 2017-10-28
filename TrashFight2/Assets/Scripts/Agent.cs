4810130cf8c0b132704db9644f877e7cd72d7dc8
John Miller <jamiller40@hotmail.com>
2017-10-28 12:18:31 -0400

    Added movement and combat volumes

---
+++ TrashFight2/Assets/Scripts/Agent.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+    /*Any object in the game that moves or needs to be effected by physics or ablilities must
+    have an attatched Agent component. It is used to move objects. That's pretty much it*/
+
+
+    public bool debugAcceleration, debugVelocity, moveable;
+
+    public bool clampVelocity;
+    public bool clampAcceleration;
+
+    public float dragForce;
+    public float mass;
+
+    public float maxVelocity;
+    public float maxAcceleration;
+
+    private Vector3 velocity;
+    private Vector3 drag;
+    private Vector3 acceleration;
+
+    private float origYPos;
+    public bool lockYPos;
+    public bool horizRot;
+
+    /*The actual numbers required to move agents using forces when couple with 
+    delta time, are very large, so for the sake of clean, easy to digest numbers, the
+    values we input will be small, then will be multiplied by this modifier whenever they are used.*/
+    public static float globalForcesModifier = 500;
+
+    /*Is set to true when a player enters a collision. this value is sent to various
+    abilities that need to know but cannot detect on their own*/
+    private bool enterCollisionFlag;
+
+    #region Properties
+
+    public Vector3 Velocity {
+        get {
+            return velocity;
+        }
+
+        set {
+            velocity = value;
+        }
+    }
+
+    public Vector3 Drag {
+        get {
+            return drag;
+        }
+
+        set {
+            drag = value;
+        }
+    }
+
+    public Vector3 Acceleration {
+        get {
+            return acceleration;
+        }
+
+        set {
+            acceleration = value;
+        }
+    }
+
+    public bool EnterCollisionFlag {
+        get {
+            return enterCollisionFlag;
+        }
+
+        set {
+            enterCollisionFlag = value;
+        }
+    }
+    #endregion
+
+    void Awake() {
+        dragForce *= globalForcesModifier;
+        mass *= globalForcesModifier;
+
+        maxVelocity *= globalForcesModifier;
+        maxAcceleration *= globalForcesModifier;
+    }
+    void Start() {
+        origYPos = transform.position.y;
+    }
+
+    /*Update method specific to this class. 
+    Is called by various Ability Objects manually. Don't forget to do this*/
+    public void AgentUpdate() {
+        if (!moveable) { return; }
+
+        ShowDebugLines();
+        ResetVectors();
+
+        if (clampAcceleration) { ClampAcceleration(); }
+
+        CalcVelocity();
+
+        if (clampVelocity) { ClampVelocity(); }
+    }
+
+    /*Handles applying calculated forces to the object's position.
+    Should always be called after AgentUpdate. This basically exists
+    so that objects can apply forces to themselves after other things have 
+    been calculated. It's useful, alright.*/
+    public void AgentLateUpdate() {
+        if (moveable) {
+            AddVelocity();
+        }
+
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        LockYPos();
+        LockYRot();
+
+        enterCollisionFlag = false;
+    }
+    void LockYRot() {
+        if (horizRot) transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+    }
+    void LockYPos() {
+        if (lockYPos) {
+            if (transform.position.y != origYPos) {
+                transform.position = new Vector3(transform.position.x, origYPos, transform.position.z);
+            }
+        }
+    }
+    void ShowDebugLines() {
+        if (debugVelocity) Debug.DrawLine(transform.position, velocity, Color.green);
+        if (debugAcceleration) Debug.DrawLine(transform.position, acceleration, Color.yellow);
+    }
+
+    //Addes net acceleration forces to velocity and smooths based on framrate
+    void CalcVelocity() {
+        CalcDrag();
+        Velocity += Acceleration * Time.deltaTime;
+    }
+
+    //Applies movement to the object
+    void AddVelocity() {
+        transform.position += Velocity;
+    }
+
+    public void ResetVectors() {
+        Velocity = Vector3.zero;
+        Drag = Vector3.zero;
+    }
+    void CalcDrag() {
+        Acceleration = Acceleration * dragForce;
+    }
+    void ClampVelocity() {
+        if (clampVelocity) Velocity = Vector3.ClampMagnitude(Velocity, maxVelocity);
+    }
+    void ClampAcceleration() {
+        if (clampAcceleration) Acceleration = Vector3.ClampMagnitude(Acceleration, maxAcceleration);
+    }
+
+    public void AddRandomForce(float _forceScale) {
+        Vector3 randomForce = _forceScale * (new Vector3(Random.Range(0, 1), 0, Random.Range(0, 1)));
+        acceleration += randomForce;
+    }
+    #region Collisions
+    void OnCollisionEnter() {
+        enterCollisionFlag = true;
+    }
+    #endregion
+}
---
+++ TrashFight2/Assets/Scripts/Player.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public enum ATTACK { NONE, PUNCH, KICK, THROW, DASH}
+    public enum MOVE { IDLE, RIGHT, LEFT }
+
+    private ATTACK attackState;
+    private MOVE moveState;
+
+    public float moveSpeed;
+
+    public float dashSpeed;
+    public float dashDuration;
+    private bool dashActive;
+
+    public float punchDamage;
+    public float punchDuration;
+    public GameObject punchVolume;
+    private bool punchActive;
+
+    public float kickDamage;
+    public float kickDuration;
+    private bool kickActive;
+    public GameObject kickVolume;
+
+    public float throwDamage;
+    public float throwDuration;
+    private bool throwActive;
+    public GameObject projectilePrefab;
+
+    private float health;
+    public float maxHealth;
+
+    [Range(0,1)]
+    public int playerID;
+
+    public Player opponent;
+    public Transform centerPoint;
+
+    private float lastAttackTime;
+
+    public int myAbilityVolumeLayer;
+
+    public bool alive = true;
+	
+    void Awake() {
+        health = maxHealth;
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if(alive) {
+            CheckAbilities();
+            CheckMove();
+        }
+        CheckDead();
+    }
+    private void CheckDead() {
+        if(!alive) {
+            foreach(MeshRenderer rend in transform.GetComponentsInChildren<MeshRenderer>()) {
+                rend.enabled = false;
+            }
+        }
+    }
+    private void CheckAbilities() {
+        //If there's no attack active, we can attack
+        if(attackState == ATTACK.NONE) {
+            if (playerID == 0) {
+                if (Input.GetKey(KeyCode.F)) {
+                    Punch();
+                } else if (Input.GetKey(KeyCode.G)) {
+                    Kick();
+                } else if (Input.GetKey(KeyCode.H)) {
+                    Throw();
+                }
+
+            } else {
+                if (Input.GetKey(KeyCode.J)) {
+                    Punch();
+                } else if (Input.GetKey(KeyCode.K)) {
+                    Kick();
+                } else if (Input.GetKey(KeyCode.L)) {
+                    Throw();
+                }
+            }
+
+        } else {
+        //If there is an attacka active, we need to check its active timer
+            switch (attackState) {
+                case ATTACK.PUNCH:
+                    if(TimerComplete(lastAttackTime, punchDuration)){
+                        attackState = ATTACK.NONE;
+                    }
+                    break;
+                case ATTACK.KICK:
+                    if (TimerComplete(lastAttackTime, kickDuration)) {
+                        attackState = ATTACK.NONE;
+                    }
+                    break;
+                case ATTACK.THROW:
+                    if (TimerComplete(lastAttackTime, throwDuration)) {
+                        attackState = ATTACK.NONE;
+                    }
+                    break;
+                case ATTACK.DASH:
+                    if (TimerComplete(lastAttackTime, dashDuration)) {
+                        attackState = ATTACK.NONE;
+                    }
+                    break;
+            }
+
+            if(attackState == ATTACK.NONE) {
+                punchVolume.SetActive(false);
+                kickVolume.SetActive(false);
+            }
+        }
+    }
+    private bool TimerComplete(float _lastTime, float _duration) {
+        if (Time.time > _lastTime + _duration) {
+            return true;
+        }
+        return false;
+    }
+    private void CheckMove() {
+        float moveAmount = 0;
+
+        if(playerID == 0) {
+            if (Mathf.Abs(Input.GetAxis("Player0Horiz")) > 0) { moveAmount = -moveSpeed * Input.GetAxis("Player0Horiz"); }
+        } else {
+            if (Mathf.Abs(Input.GetAxis("Player1Horiz")) > 0) { moveAmount = -moveSpeed * Input.GetAxis("Player1Horiz"); }
+        }
+
+        if(moveAmount > 0) {
+            //Turn right
+            transform.localRotation = Quaternion.Euler(new Vector3(0,-90,0));
+        } else if(moveAmount < 0) {
+            //Turn left
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 90, 0));
+        }
+
+        centerPoint.rotation = Quaternion.Euler(new Vector3(0, centerPoint.eulerAngles.y + moveAmount, 0));
+    }
+
+    private void Dash() {
+        attackState = ATTACK.DASH;
+        lastAttackTime = Time.time;
+    }
+    private void Punch() {
+        attackState = ATTACK.PUNCH;
+        lastAttackTime = Time.time;
+        punchVolume.SetActive(true);
+    }
+    private void Kick() {
+        attackState = ATTACK.KICK;
+        lastAttackTime = Time.time;
+        kickVolume.SetActive(true);
+    }
+    private void Throw() {
+        attackState = ATTACK.THROW;
+        lastAttackTime = Time.time;
+    }
+    private void TakeDamage(float _damage) {
+        health -= _damage;
+        if(health <= 0) {
+            Die();
+        }
+    }
+    private void Die() {
+        print("Player " + playerID  + " died");
+        alive = false;
+    }
+    private void OnTriggerEnter(Collider _other) {
+        print("Triggered!!");
+        if(_other.gameObject.layer != myAbilityVolumeLayer) {
+            switch (_other.tag) {
+                case "PunchVolume":
+                    TakeDamage(punchDamage);
+                    break;
+                case "KickVolume":
+                    TakeDamage(kickDamage);
+                    break;
+            }
+        }
+    }
+}
